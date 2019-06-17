c384f1b1a8d0b2c598483625aef6ca1c10b208d5
Valentina Heimann <vheimann2@googlemail.com>
2019-06-17 17:01:52 +0300

    DONE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+10.times do
+  article_new = Article.new(title: Faker::Book.title, content: Faker::ChuckNorris.fact)
+  article_new.save
+end
